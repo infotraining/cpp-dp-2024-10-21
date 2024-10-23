9deee3e942cb64aee05d5a51b0df1ba34008f313
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-10-23 09:15:09 +0200

    Update

--- Structural/Composite.Exercise/shape_group.cpp
+++ Structural/Composite.Exercise/shape_group.cpp
@@ -4,3 +4,46 @@
 
 using namespace std;
 using namespace Drawing;
+
+namespace
+{
+    bool is_registered = SingletonShapeFactory::instance().register_creator(
+        ShapeGroup::id, &std::make_unique<ShapeGroup>);
+}
+
+ShapeGroup::ShapeGroup(const ShapeGroup& source)
+{
+    std::transform(source.shapes_.begin(), source.shapes_.end(),
+        std::back_inserter(shapes_),
+        [](const auto& shp) { return shp->clone(); });
+}
+
+ShapeGroup& ShapeGroup::operator=(const ShapeGroup& source)
+{
+    ShapeGroup temp(source);
+    swap(temp);
+
+    return *this;
+}
+
+void ShapeGroup::add(std::unique_ptr<Shape> shp)
+{
+    shapes_.push_back(std::move(shp));
+}
+
+void ShapeGroup::move(int x, int y)
+{
+    for (const auto& shp : shapes_)
+        shp->move(x, y);
+}
+
+void ShapeGroup::draw() const
+{
+    for (const auto& shp : shapes_)
+        shp->draw();
+}
+
+void ShapeGroup::swap(ShapeGroup& other)
+{
+    shapes_.swap(other.shapes_);
+}
--- Structural/Composite.Exercise/shape_group.hpp
+++ Structural/Composite.Exercise/shape_group.hpp
@@ -8,9 +8,58 @@
 
 namespace Drawing
 {
-    // TODO - implement a composite for shapes
-    class ShapeGroup
+    class ShapeGroup : public CloneableShape<ShapeGroup>
     {
+        std::vector<std::unique_ptr<Shape>> shapes_;
+
+        void swap(ShapeGroup& other);
+
+    public:
+        static constexpr const char* id = "ShapeGroup";
+
+        using iterator = std::vector<std::unique_ptr<Shape>>::iterator;
+        using const_iterator = std::vector<std::unique_ptr<Shape>>::const_iterator;
+
+        ShapeGroup() = default;
+
+        ShapeGroup(const ShapeGroup& source);
+
+        ShapeGroup& operator=(const ShapeGroup& source);
+
+        ShapeGroup(ShapeGroup&&) noexcept = default;
+
+        ShapeGroup& operator=(ShapeGroup&&) noexcept = default;
+
+        void add(std::unique_ptr<Shape> shp);
+
+        void move(int x, int y) override;
+
+        void draw() const override;
+
+        iterator begin()
+        {
+            return shapes_.begin();
+        }
+
+        const_iterator begin() const
+        {
+            return shapes_.begin();
+        }
+
+        iterator end()
+        {
+            return shapes_.end();
+        }
+
+        const_iterator end() const
+        {
+            return shapes_.end();
+        }
+
+        size_t size() const
+        {
+            return shapes_.size();
+        }
     };
 }
 
