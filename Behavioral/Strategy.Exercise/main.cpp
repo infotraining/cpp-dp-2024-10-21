867d5f279768078ed6ca6678a4f18cc26221a61a
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-10-23 13:48:33 +0200

    Strategy

--- Behavioral/Strategy.Exercise/main.cpp
+++ Behavioral/Strategy.Exercise/main.cpp
@@ -23,23 +23,90 @@ struct StatResult
 using Data = std::vector<double>;
 using Results = std::vector<StatResult>;
 
-enum StatisticsType
+// enum StatisticsType
+// {
+//     avg,
+//     min_max,
+//     sum
+// };
+
+class Algorithm
 {
-    avg,
-    min_max,
-    sum
+public:
+    virtual Results calculate(Data& data) = 0;
+    virtual ~Algorithm() = default;
+};
+
+class AvgAlgorithm : public Algorithm
+{
+    Results calculate(Data& data) override
+    {    
+        double sum = std::accumulate(data.begin(), data.end(), 0.0);
+        double avg = sum / data.size();
+     
+        return Results{StatResult{"Avg", avg}}; // r-value
+    }
+};
+
+class MinMaxAlgorithm : public Algorithm
+{
+    Results calculate(Data& data) override
+    {
+        Results results;
+        double min = *(std::min_element(data.begin(), data.end()));
+        double max = *(std::max_element(data.begin(), data.end()));
+
+        results.push_back(StatResult("Min", min));
+        results.push_back(StatResult("Max", max));
+        return results;
+    }
+};
+
+class SumAlgorithm : public Algorithm
+{
+    Results calculate(Data& data) override
+    {
+        Results results;
+        double sum = std::accumulate(data.begin(), data.end(), 0.0);
+
+        results.push_back(StatResult("Sum", sum));
+        return results;
+    }
+};
+
+class CompositeAlgorithm : public Algorithm
+{
+    std::vector<std::shared_ptr<Algorithm>> stats_;
+public:
+    void add_statistics(std::shared_ptr<Algorithm> stat)
+    {
+        stats_.push_back(stat);
+    }
+
+    Results calculate(Data& data) override
+    {
+        Results results{};
+
+        for(const auto& stat : stats_)
+        {
+            Results tmp_result = stat->calculate(data);
+            results.insert(results.end(), tmp_result.begin(), tmp_result.end());
+        }
+
+        return results;
+    }
 };
 
 class DataAnalyzer
-{
-    StatisticsType stat_type_;
+{    
+    std::shared_ptr<Algorithm> algorithm_;
     Data data_;
     Results results_;
 
 public:
-    DataAnalyzer(StatisticsType stat_type)
-        : stat_type_{stat_type}
-    {
+    DataAnalyzer(std::shared_ptr<Algorithm> stat)
+        : algorithm_{stat}
+    {        
     }
 
     void load_data(const std::string& file_name)
@@ -60,35 +127,15 @@ public:
         std::cout << "File " << file_name << " has been loaded...\n";
     }
 
-    void set_statistics(StatisticsType stat_type)
+    void set_statistics(std::shared_ptr<Algorithm> stat)
     {
-        stat_type_ = stat_type;
+        algorithm_ = stat;
     }
 
     void calculate()
     {
-        if (stat_type_ == avg)
-        {
-            double sum = std::accumulate(data_.begin(), data_.end(), 0.0);
-            double avg = sum / data_.size();
-
-            StatResult result("Avg", avg);
-            results_.push_back(result);
-        }
-        else if (stat_type_ == min_max)
-        {
-            double min = *(std::min_element(data_.begin(), data_.end()));
-            double max = *(std::max_element(data_.begin(), data_.end()));
-
-            results_.push_back(StatResult("Min", min));
-            results_.push_back(StatResult("Max", max));
-        }
-        else if (stat_type_ == sum)
-        {
-            double sum = std::accumulate(data_.begin(), data_.end(), 0.0);
-
-            results_.push_back(StatResult("Sum", sum));
-        }
+        Results result = algorithm_->calculate(data_);
+        results_.insert(results_.end(), result.begin(), result.end());
     }
 
     const Results& results() const
@@ -105,17 +152,18 @@ void show_results(const Results& results)
 
 int main()
 {
-    DataAnalyzer da{avg};
+    auto avg = std::make_shared<AvgAlgorithm>();
+    auto min_max = std::make_shared<MinMaxAlgorithm>();
+    auto sum = std::make_shared<SumAlgorithm>();
+
+    auto std_stats = std::make_shared<CompositeAlgorithm>();
+    std_stats->add_statistics(avg);
+    std_stats->add_statistics(min_max);
+    std_stats->add_statistics(sum);
+
+    DataAnalyzer da{std_stats};
     da.load_data("stats_data.dat");
-
-    da.calculate();
-
-    da.set_statistics(min_max);
-    da.calculate();
-
-    da.set_statistics(sum);
-    da.calculate();
-
+    da.calculate();    
     show_results(da.results());
 
     std::cout << "\n\n";
